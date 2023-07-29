fc8665fb3c3053a7643e691d5759d920cb6935fe
Sushil Soniwal <codersushil3@gmail.com>
2023-07-29 09:29:21 +0530

    LC: 83. Remove Duplicates from Sorted List

--- P_85_Remove_Duplicates_From_Sorted_List.java
+++ P_85_Remove_Duplicates_From_Sorted_List.java
@@ -1,19 +1,4 @@
-class ListNode {
-    public int val;
-    public ListNode next;
-
-    public ListNode() {
-    }
-
-    public ListNode(int val) {
-        this.val = val;
-    }
-
-    public ListNode(int val, ListNode next) {
-        this.val = val;
-        this.next = next;
-    }
-}
+import Classes.ListNode;
 
 public class P_85_Remove_Duplicates_From_Sorted_List {
 
