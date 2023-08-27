7c6edf51bcd2c3ab9df8911fcb5d417b7833bec5
Sushil Soniwal <codersushil3@gmail.com>
2023-08-27 17:27:50 +0530

    GFG: Maximum sum leaf to root path

---
+++ P_157_Maximum_Root_Leaf_Sum.java
@@ -0,0 +1,39 @@
+import TreeClasses.Node;
+
+public class P_157_Maximum_Root_Leaf_Sum {
+
+    // https://practice.geeksforgeeks.org/problems/maximum-sum-leaf-to-root-path/1
+
+    // ! T.C = O(n) & S.C = O(h).
+    public static int maxPathSum(Node root) {
+        return sumNumbersHelper(root, 0);
+    }
+
+    private static int sumNumbersHelper(Node node, int currentSum) {
+        if (node == null) {
+            return Integer.MIN_VALUE;
+        }
+
+        currentSum = currentSum + node.val; // Update the current sum.
+
+        // Check if it's a leaf node (both left and right are null).
+        if (node.left == null && node.right == null) {
+            return currentSum; // Return the current sum for leaf nodes.
+        }
+
+        // Recursively calculate the sum for left and right subtrees.
+        int leftSum = sumNumbersHelper(node.left, currentSum);
+        int rightSum = sumNumbersHelper(node.right, currentSum);
+
+        // Return the maximum sum of left and right subtrees.
+        return Math.max(leftSum, rightSum);
+    }
+
+    public static void main(String[] args) {
+        Node root = new Node(10);
+        root.right = new Node(-20);
+        root.right.left = new Node(-30);
+
+        System.out.println(maxPathSum(root));
+    }
+}
