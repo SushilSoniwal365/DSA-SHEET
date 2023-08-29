03468887a90a3bbe79ede3ec3e878312021c23e2
Sushil Soniwal <codersushil3@gmail.com>
2023-08-29 10:26:23 +0530

    LC: 437. Path Sum III

---
+++ P_162_Path_With_Sum_K.java
@@ -0,0 +1,49 @@
+import java.util.HashMap;
+
+import TreeClasses.TreeNode;
+
+public class P_162_Path_With_Sum_K {
+
+    // https://leetcode.com/problems/path-sum-iii/description/
+
+    // ! T.C = O(n) & S.C = O(h).
+    static int total;
+
+    public static int pathSum(TreeNode root, int sum) {
+        total = 0;
+        if (root == null) {
+            return 0;
+        }
+        HashMap<Long, Long> map = new HashMap<>();
+        map.put((long) 0, (long) 1);
+        solve(root, 0, sum, map);
+        return total;
+    }
+
+    private static void solve(TreeNode root, long currSum, long targetSum, HashMap<Long, Long> map) {
+        if (root == null)
+            return;
+        currSum += (long) root.val;
+        if (map.containsKey(currSum - targetSum)) {
+            total += map.get(currSum - targetSum);
+        }
+        map.put(currSum, map.getOrDefault(currSum, (long) 0) + 1);
+        solve(root.left, currSum, targetSum, map);
+        solve(root.right, currSum, targetSum, map);
+        map.put(currSum, map.get(currSum) - 1);
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(3);
+        root.left = new TreeNode(5);
+        root.left.left = new TreeNode(6);
+        root.left.right = new TreeNode(2);
+        root.left.right.left = new TreeNode(7);
+        root.left.right.right = new TreeNode(4);
+        root.right = new TreeNode(1);
+        root.right.left = new TreeNode(0);
+        root.right.right = new TreeNode(8);
+
+        System.out.println(pathSum(root, 8));
+    }
+}
