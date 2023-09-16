3ae2e909aee7e15d3cbfba61ae9cc92dfdae7225
Sushil Soniwal <codersushil3@gmail.com>
2023-09-16 18:33:30 +0530

    LC: 100. Same Tree

---
+++ P_161_Check_Identical_Trees.java
@@ -0,0 +1,32 @@
+import TreeClasses.TreeNode;
+
+public class P_161_Check_Identical_Trees {
+
+    // https://leetcode.com/problems/same-tree/
+
+    // ? T.C = O(n) & S.C = O(n)
+    public static boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        }
+        if ((p == null && q != null) || (p != null && q == null)) {
+            return false;
+        }
+        if (p.val != q.val) {
+            return false;
+        }
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        root.left.left = new TreeNode(4);
+        root.left.right = new TreeNode(5);
+        root.right.left = new TreeNode(6);
+        root.right.right = new TreeNode(7);
+
+        System.out.println(isSameTree(root, root));
+    }
+}
