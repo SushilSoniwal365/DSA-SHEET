c02c0a87ea8a84ec80d09b406f6d1df7f610bebb
Sushil Soniwal <codersushil3@gmail.com>
2023-09-21 09:09:10 +0530

    LC: 938. Range Sum of BST

---
+++ P_191_Range_Sum_Of_BST.java
@@ -0,0 +1,33 @@
+import TreeClasses.TreeNode;
+
+public class P_191_Range_Sum_Of_BST {
+
+    // https://leetcode.com/problems/range-sum-of-bst/description/
+
+    // ? T.C = O(n) & S.C = O(n).
+    public static int rangeSumBST(TreeNode root, int low, int high) {
+        if (root == null) {
+            return 0;
+        }
+        if (root.val < low) {
+            return rangeSumBST(root.right, low, high);
+        } else if (root.val > high) {
+            return rangeSumBST(root.left, low, high);
+        }
+        return root.val + rangeSumBST(root.left, low, high) + rangeSumBST(root.right, low, high);
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(6);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(8);
+        root.left.left = new TreeNode(0);
+        root.left.right = new TreeNode(4);
+        root.right.left = new TreeNode(7);
+        root.right.right = new TreeNode(9);
+        root.left.right.left = new TreeNode(3);
+        root.left.right.right = new TreeNode(5);
+
+        System.out.println(rangeSumBST(root, 2, 8));
+    }
+}
